89aa9582f90d51f4857d33fd5697faf3b8dfa75e
Mauricio Brito Neto <mauricio2006k@outlook.com>
2026-08-20 22:19:36 -0300

    aula 20-08

--- introducao-vite/src/pages/atividades/index.jsx
+++ introducao-vite/src/pages/atividades/index.jsx
@@ -1,7 +1,10 @@
-export default function Atividades() {
+import { Link } from "react-router";
+
+export default function Atividades () {
     return (
         <div>
             <h1>Atividades</h1>
+            <Link to="/exemplos/1">Exemplo 1</Link>  
         </div>
-    )
+    );
 }
